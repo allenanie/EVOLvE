4d456ceed88cf1ffaabdab328a5130db6a134dac
windweller <leo.niecn@gmail.com>
2025-01-15 21:57:38 -0800

    update test file, add __init__ to sampling module

--- tests/test_movielens.py
+++ tests/test_movielens.py
@@ -1,7 +1,7 @@
 from banditbench.tasks.cb.movielens import MovieLens, MovieLensVerbal
 
 def test_construct_env():
-    env = MovieLens('100k-ratings', 5, 20, 5, 'train', save_data_dir='/piech/u/anie/tensorflow_datasets/')
+    env = MovieLens('100k-ratings', 5, 20, 5, 'train', save_data_dir='./tensorflow_datasets/')
     state, _ = env.reset()
     verbal_env = MovieLensVerbal(env)
     verbal_state, info = verbal_env.reset()
